640843c5037b5aa13a6cdc40c2be528d82f0892b
Tom Gardham-Pallister <thomas.gardham-pallister@news.co.uk>
2018-08-08 13:50:04 +0530

    made world gallery

--- vr-test/components/section.js
+++ vr-test/components/section.js
@@ -14,23 +14,20 @@ AFRAME.registerComponent('section', {
         }).done(function (response) {
             var count = 0;
             $.each(response, function (i, item) {
-                if (count < 4) {
-
-                    document.querySelector('#row-one-image-' + i).setAttribute('src', 'http://hackathon-image-proxy.azurewebsites.net/?url=' + item.images.mobile.src_url);
-                    //<a-link href="index.html" title="My Homepage" image="#homeThumbnail"></a-link>
-                    $('#row-one').append('<a-link href="index.html" title="' + item.title + '" image="#row-one-image-'+i+'"></a-link>');
-
-                    // $('#row-one').append('<a-image src="#row-one-image-' + i + '"></a-image>');
-                    // $('#row-one').append('<a-entity text="color: black; value: ' + item.title + ';"></a-entity>');
-                    console.log(item);
-                }
-                if (count >= 4 && count < 8) {
-                    document.querySelector('#row-two-image-' + i).setAttribute('src', 'http://hackathon-image-proxy.azurewebsites.net/?url=' + item.images.mobile.src_url);
-                    $('#row-two').append('<a-image src="#row-two-image-' + i + '"></a-image>');
-                    $('#row-two').append('<a-entity text="color: black; value: ' + item.title + ';"></a-entity>');
-                    console.log(item);
+                if(item.images.mobile.src_url !== undefined){
+                    if (count < 3) {
+                        document.querySelector('#row-one-image-' + i).setAttribute('src', 'http://hackathon-image-proxy.azurewebsites.net/?url=' + item.images.mobile.src_url);
+                        $('#row-one').append('<a-image src="#row-one-image-' + i + '" geometry="primitive: box; height: 1; width: 2; depth: 1;"><a-entity position="0 0.3 -0.2" text="color: white; value: ' + item.title + ';"></a-entity></a-image>');
+                        $('#row-one').append('<a-entity text="color: black; value: "";"></a-entity>');
+                    }
+                    if (count >= 4 && count < 7) {
+                        document.querySelector('#row-two-image-' + i).setAttribute('src', 'http://hackathon-image-proxy.azurewebsites.net/?url=' + item.images.mobile.src_url);
+                        $('#row-two').append('<a-image src="#row-two-image-' + i + '" geometry="primitive: box; height: 1; width: 2; depth: 1;"><a-entity position="0 0.3 -0.2" text="color: white; value: ' + item.title + ';"></a-entity></a-image>');
+                        $('#row-two').append('<a-entity text="color: black; value: "";"></a-entity>');
+                    }
                 }
                 count++;
+                console.log(item);
             })
             self.data.message = 'lol';
             console.log(self.data.message);
